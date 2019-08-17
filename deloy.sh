512bf8504664ac231b4d3c3674947712e0f2a35f
hamlin <hamlin@lednet.com>
2019-08-17 18:50:13 +0800

    config

--- deloy.sh
+++ deloy.sh
@@ -7,10 +7,11 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd public
 
 git init
 git add -A
 git commit -m 'deploy'
 
+git push -f git@github.com:ChaselLHL/myBlog.git master:gh-pages
 cd -
